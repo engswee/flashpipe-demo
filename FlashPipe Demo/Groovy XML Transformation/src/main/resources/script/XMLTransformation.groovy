59ad519d3b9574ffdcf07619812fb4672177c061
Eng Swee Yeoh <engswee@users.noreply.github.com>
2024-02-23 11:46:25 +0000

    Add logic for value mapping, and include unit test

--- FlashPipe Demo/Groovy XML Transformation/src/main/resources/script/XMLTransformation.groovy
+++ FlashPipe Demo/Groovy XML Transformation/src/main/resources/script/XMLTransformation.groovy
@@ -1,33 +1,37 @@
 import com.sap.gateway.ip.core.customdev.util.Message
+import com.sap.it.api.ITApiFactory
+import com.sap.it.api.mapping.ValueMappingApi
 import groovy.xml.MarkupBuilder
 
 import java.text.SimpleDateFormat
 
 def Message processData(Message message) {
-  Reader reader = message.getBody(Reader)
-  def Order = new XmlSlurper().parse(reader)
-  Writer writer = new StringWriter()
-  def indentPrinter = new IndentPrinter(writer, '    ')
-  def builder = new MarkupBuilder(indentPrinter)
+    Reader reader = message.getBody(Reader)
+    def Order = new XmlSlurper().parse(reader)
+    Writer writer = new StringWriter()
+    def builder = new MarkupBuilder(writer)
 
-  def items = Order.Item.findAll { it.Valid.text() == 'true' }
-  builder.PurchaseOrder {
-    'Header' {
-      'ID' Order.Header.OrderNumber
-      'DocumentDate' new SimpleDateFormat('yyyy-MM-dd').format(new SimpleDateFormat('yyyyMMdd').parse(Order.Header.Date.text()))
-      if (!items.size())
-        'DocumentType' message.getProperty('DocType')
+    def sourceDocType = message.getProperty('DocType')
+    ValueMappingApi api = ITApiFactory.getService(ValueMappingApi, null)
+
+    def items = Order.Item.findAll { it.Valid.text() == 'true' }
+    builder.PurchaseOrder {
+        'Header' {
+            'ID' Order.Header.OrderNumber
+            'DocumentDate' new SimpleDateFormat('yyyy-MM-dd').format(new SimpleDateFormat('yyyyMMdd').parse(Order.Header.Date.text()))
+            if (!items.size())
+                'DocumentType' api.getMappedValue('S4', 'DocType', sourceDocType, 'ACME', 'DocumentType')
+        }
+
+        items.each { item ->
+            'Item' {
+                'ItemNumber' item.ItemNumber.text().padLeft(3, '0')
+                'ProductCode' item.MaterialNumber
+                'Quantity' item.Quantity
+            }
+        }
     }
 
-    items.each { item ->
-      'Item' {
-        'ItemNumber' item.ItemNumber.text().padLeft(3, '0')
-        'ProductCode' item.MaterialNumber
-        'Quantity' item.Quantity
-      }
-    }
-  }
-
-  message.setBody(writer.toString())
-  return message
+    message.setBody(writer.toString())
+    return message
 }
--- FlashPipe Demo/Groovy XML Transformation/src/test/groovy/XMLTransformationSpec.groovy
+++ FlashPipe Demo/Groovy XML Transformation/src/test/groovy/XMLTransformationSpec.groovy
@@ -1,4 +1,5 @@
 import com.sap.gateway.ip.core.customdev.util.Message
+import com.sap.it.api.mapping.ValueMappingApi
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -18,26 +19,43 @@ class XMLTransformationSpec extends Specification {
 
     def 'Scenario 1 - Order has items'() {
         given: 'the message body is initialized'
-        def msgBody = this.getClass().getResource('input1.xml').newInputStream()
-        msg.setBody(msgBody)
+        msg.setBody(this.getClass().getResource('input1.xml').newInputStream())
 
         when: 'we execute the Groovy script'
         script.processData(msg)
 
         then: 'the output message body is as expected'
-        msg.getBody(String) == this.getClass().getResource('output1.xml').text.normalize()
+        def root = new XmlSlurper().parse(msg.getBody(Reader))
+        verifyAll {
+            root.Header.ID.text() == 'ORD60001'
+            root.Header.DocumentDate.text() == '2019-02-18'
+            root.Header.DocumentType.text() == ''
+            root.Item.size() == 1
+            root.Item.ItemNumber.text() == '010'
+            root.Item.ProductCode.text() == 'MT70001'
+            root.Item.Quantity.text() == '57'
+        }
     }
 
     def 'Scenario 2 - Order does not have items'() {
         given: 'the message body and property are initialized'
-        def msgBody = this.getClass().getResource('input2.xml').newInputStream()
-        msg.setBody(msgBody)
+        msg.setBody(this.getClass().getResource('input2.xml').newInputStream())
         msg.setProperty('DocType', 'HDR')
+
+        // Set up value mapping entries
+        ValueMappingApi vmapi = ValueMappingApi.getInstance()
+        vmapi.addEntry('S4', 'DocType', 'HDR', 'ACME', 'DocumentType', 'ACME-HDR')
 
         when: 'we execute the Groovy script'
         script.processData(msg)
 
         then: 'the output message body is as expected'
-        msg.getBody(String) == this.getClass().getResource('output2.xml').text.normalize()
+        def root = new XmlSlurper().parse(msg.getBody(Reader))
+        verifyAll {
+            root.Header.ID.text() == 'ORD80002'
+            root.Header.DocumentDate.text() == '2020-02-18'
+            root.Header.DocumentType.text() == 'ACME-HDR'
+            root.Item.size() == 0
+        }
     }
 }
